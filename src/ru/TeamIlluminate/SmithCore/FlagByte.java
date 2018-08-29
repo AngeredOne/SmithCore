9c53ac987084e30d34c758a15f7bb9726bea0ff2
The_Nex <the.nex.official@gmail.com>
2018-08-29 18:32:26 +0800

    ByteStreaming ended

--- src/ru/TeamIlluminate/SmithCore/FlagByte.java
+++ src/ru/TeamIlluminate/SmithCore/FlagByte.java
@@ -4,10 +4,16 @@ public class FlagByte {
 
     public boolean Disconnect = false;
     public boolean Resended = false;
-    public boolean empty0 = false;
-    public boolean emtpy1 = false;
-    public boolean empty2 = false;
-    public boolean empty3 = false;
-    public boolean empty4 = false;
     public boolean EndTransmission = false;
+    public boolean Empty = false;
+
+    public byte[] getBytes()
+    {
+        byte[] bytes = new byte[4];
+        bytes[0] = (byte) (Disconnect?1:0);
+        bytes[1] = (byte) (Resended?1:0);
+        bytes[2] = (byte) (EndTransmission?1:0);
+        bytes[3] = (byte) (Empty?1:0);
+        return bytes;
+    }
 }
