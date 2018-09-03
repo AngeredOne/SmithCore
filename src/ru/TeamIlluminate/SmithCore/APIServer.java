f43a45f33a4a7a4c2d8b8814201c5ec0164b5f58
The_Nex <the.nex.official@gmail.com>
2018-09-03 17:41:23 +0800

    ServerSide logic processed

--- src/ru/TeamIlluminate/SmithCore/APIServer.java
+++ src/ru/TeamIlluminate/SmithCore/APIServer.java
@@ -1,4 +1,6 @@
 package ru.TeamIlluminate.SmithCore;
 
 public class APIServer {
+
+    public static int max_Timeout = 180;
 }
--- src/ru/TeamIlluminate/SmithCore/ServerAgent.java
+++ src/ru/TeamIlluminate/SmithCore/ServerAgent.java
@@ -2,54 +2,93 @@ package ru.TeamIlluminate.SmithCore;
 
 import java.io.IOException;
 import java.net.Socket;
+
 import ru.TeamIlluminate.SmithCore.StateManager.codes;
 
- class ServerAgent {
+class ServerAgent {
     private Socket socket;
-    public Socket getSocket() {return socket;}
+
+    public Socket getSocket() {
+        return socket;
+    }
+
     private NetworkStream stream;
     private SmithProtocol protocol;
     private String UID;
+    private ReconnectSystem rSys;
+
     public boolean isConnected;
 
-    public ServerAgent (Socket socket, String UID) throws IOException {
+    public ServerAgent(Socket socket, String UID) throws IOException {
         this.socket = socket;
         this.UID = UID;
         this.protocol = new SmithProtocol(new NetworkStream(socket.getInputStream(), socket.getOutputStream()));
+        this.rSys = new ReconnectSystem(this);
     }
 
-    public void initSend(Byte[] data)
-    {
-         codes code = protocol.send(data);
-         if(code == codes.SendException)
-         {
-             //Call connectHandler
-         }
+    public void initSend(Byte[] data) {
+        codes code = protocol.send(data);
+        if (code == codes.SendException) {
+            rSys.start();
+        }
     }
 
-    public void initRecieve()
-    {
+    public void initRecieve() {
         codes code = protocol.recieve();
 
-        if(code == codes.ReceiveException)
-        {
-            //Call connectHandler
-        }
-        else if(code == codes.DissconectionFlag)
-        {
+        if (code == codes.ReceiveException) {
+            rSys.start();
+        } else if (code == codes.DissconectionFlag) {
             //Call delete agent
         }
     }
 
 
-    private void agentDisconnected()
-    {
+    private void agentDisconnected() {
 
+    }
+
+}
+
+class ReconnectSystem extends Thread
+{
+
+    private ServerAgent agent = null;
+    private int timeOut = 0;
+
+    public ReconnectSystem(ServerAgent agent)
+    {
+        this.agent = agent;
+        timeOut = 0;
+    }
+
+
+    @Override
+    public void run()
+    {
+        connectHandler();
     }
 
     private void connectHandler()
     {
-
+        while(!agent.isConnected) {
+            if (timeOut < APIServer.max_Timeout * 5) // timeOut = 5 equals 1 second
+            {
+                timeOut++;
+                try {
+                    Thread.currentThread().sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    //Call SM thread error(thread is aborted but code is run)
+                }
+            }
+            else
+            {
+                //Call StateManager, agent full disconnected
+            }
+        }
+        //if agent reconnected, code will come here
+        //Call StateManager, agent reconnected
     }
 
 }
