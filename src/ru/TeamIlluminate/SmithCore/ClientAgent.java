f47f70853bd82fef89ffa92f4e7340625ca25e63
pilad0hwtts <pilad0hwtts@yandex.ru>
2018-09-09 17:43:28 +0800

    fixed ClientAgent

--- src/ru/TeamIlluminate/SmithCore/ClientAgent.java
+++ src/ru/TeamIlluminate/SmithCore/ClientAgent.java
@@ -6,33 +6,38 @@ import java.net.SocketAddress;
 
 class ClientAgent extends Agent {
 
+    private Socket socket;
+
     public ClientAgent() {
-        super(new Socket(), "");
+        super("");
+        socket = new Socket();
     }
 
     public void connect(SocketAddress server) {
         try {
             socket.connect(server);
-            stream = new NetworkStream(socket.getInputStream(), socket.getOutputStream());
         } catch (IOException e) {
-            //error when try connect
             e.printStackTrace();
         }
     }
+
     public void initSend(Byte[] data) {
         if (getConnectionStatus()) {
             protocol.Send(data);
         } else agentDisconnected();
     }
+
     public byte[] initRecieve() {
         if (getConnectionStatus()) {
             protocol.Receive();
         } else agentDisconnected();
         return null;
     }
+
     public boolean getConnectionStatus() {
         return socket.isConnected() & !socket.isClosed();
     }
+
     private void agentDisconnected() {
 
     }
