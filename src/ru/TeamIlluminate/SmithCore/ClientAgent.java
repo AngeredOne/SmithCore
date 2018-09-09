1fef7d4c7d81b9c91bf4e9f9001c88e54245fa89
The_Nex <the.nex.official@gmail.com>
2018-09-09 18:12:35 +0800

    Agents fixed, may be finally

--- src/ru/TeamIlluminate/SmithCore/ClientAgent.java
+++ src/ru/TeamIlluminate/SmithCore/ClientAgent.java
@@ -7,39 +7,74 @@ import java.net.SocketAddress;
 class ClientAgent extends Agent {
 
     private Socket socket;
-
+    private SocketAddress serverAddress;
     public ClientAgent() {
         super("");
         socket = new Socket();
     }
 
-    public void connect(SocketAddress server) {
+    public void connect(SocketAddress serverAddress) {
+
+        this.serverAddress = serverAddress;
+
         try {
-            socket.connect(server);
+            socket.connect(serverAddress);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void initSend(Byte[] data) {
-        if (getConnectionStatus()) {
-            protocol.Send(data);
-        } else agentDisconnected();
+    private void HostDisconnected()
+    {
+        new ReconnectSystem(this).start();
     }
 
-    public byte[] initRecieve() {
-        if (getConnectionStatus()) {
-            protocol.Receive();
-        } else agentDisconnected();
-        return null;
+    private void HostReconnected()
+    {
+        isConnected = true;
     }
 
-    public boolean getConnectionStatus() {
-        return socket.isConnected() & !socket.isClosed();
+    class ReconnectSystem extends Thread
+    {
+
+        private ClientAgent agent = null;
+        private int timeOut = 0;
+
+        public ReconnectSystem(ClientAgent agent)
+        {
+            this.agent = agent;
+            timeOut = 0;
+        }
+
+
+        @Override
+        public void run()
+        {
+            connectHandler();
+        }
+
+        private void connectHandler()
+        {
+            while(!agent.isConnected) {
+                if (timeOut < APIServer.max_Timeout * 5) // timeOut = 5 equals 1 second
+                {
+                    timeOut++;
+                    try {
+                        agent.socket.connect(agent.serverAddress);
+                        Thread.currentThread().sleep(200);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        //Call SM thread error(thread is aborted but code is run)
+                    }
+                }
+                else
+                {
+                    StateManager.instance().eventSystem.AgentDisconnected(agent, true);
+                }
+            }
+            //if agent reconnected, code will come here
+            StateManager.instance().eventSystem.AgentReconnected(agent);
+        }
     }
 
-    private void agentDisconnected() {
-
-    }
-    private void reconnectAttemp() {}
 }
--- src/ru/TeamIlluminate/SmithCore/ServerAgent.java
+++ src/ru/TeamIlluminate/SmithCore/ServerAgent.java
@@ -12,8 +12,6 @@ class ServerAgent extends Agent {
     private ReconnectSystem rSys;
     private Socket socket;
 
-    public boolean isConnected;
-
     public ServerAgent(String UID, Socket socket){
         super(UID);
 
@@ -24,54 +22,62 @@ class ServerAgent extends Agent {
         } catch (IOException ex) {}
 
         this.rSys = new ReconnectSystem(this);
+
+
     }
+
+     public void AgentDisconnected()
+     {
+         rSys.start();
+     }
+
 
     public Socket getAgentSocket()
     {
         return this.socket;
     }
 
-}
-
-class ReconnectSystem extends Thread
-{
-
-    private ServerAgent agent = null;
-    private int timeOut = 0;
-
-    public ReconnectSystem(ServerAgent agent)
+    class ReconnectSystem extends Thread
     {
-        this.agent = agent;
-        timeOut = 0;
-    }
+
+        private ServerAgent agent = null;
+        private int timeOut = 0;
+
+        public ReconnectSystem(ServerAgent agent)
+        {
+            this.agent = agent;
+            timeOut = 0;
+        }
 
 
-    @Override
-    public void run()
-    {
-        connectHandler();
-    }
+        @Override
+        public void run()
+        {
+            connectHandler();
+        }
 
-    private void connectHandler()
-    {
-        while(!agent.isConnected) {
-            if (timeOut < APIServer.max_Timeout * 5) // timeOut = 5 equals 1 second
-            {
-                timeOut++;
-                try {
-                    Thread.currentThread().sleep(200);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    //Call SM thread error(thread is aborted but code is run)
+        private void connectHandler()
+        {
+            while(!agent.isConnected) {
+                if (timeOut < APIServer.max_Timeout * 5) // timeOut = 5 equals 1 second
+                {
+                    timeOut++;
+                    try {
+                        Thread.currentThread().sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        //Call SM thread error(thread is aborted but code is run)
+                    }
+                }
+                else
+                {
+                    StateManager.instance().eventSystem.AgentDisconnected(agent, true);
                 }
             }
-            else
-            {
-                StateManager.instance().eventSystem.AgentDisconnected(agent, true);
-            }
+            //if agent reconnected, code will come here
+            StateManager.instance().eventSystem.AgentReconnected(agent);
         }
-        //if agent reconnected, code will come here
-        StateManager.instance().eventSystem.AgentReconnected(agent);
     }
 
 }
+
