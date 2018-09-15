06a10f115c8dbe49431796861758aeda3fa857ba
The_Nex <the.nex.official@gmail.com>
2018-09-15 16:28:41 +0800

    fix Agent

--- src/ru/TeamIlluminate/SmithCore/Agent.java
+++ src/ru/TeamIlluminate/SmithCore/Agent.java
@@ -16,22 +16,8 @@ public abstract class Agent {
         this.UID = UID;
     }
 
-    public void InitSend(Byte[] data) {
-        StateManager.RETURN_CODE code = protocol.Send(data);
-        if (code == StateManager.RETURN_CODE.SendException) {
-            StateManager.instance().eventSystem.AgentDisconnected(this, false);
-        }
-    }
-
     public void InitRecieve() {
-
-        StateManager.RETURN_CODE code = protocol.Receive();
-
-        if (code == StateManager.RETURN_CODE.ReceiveException) {
-            StateManager.instance().eventSystem.AgentDisconnected(this, false);
-        } else if (code == StateManager.RETURN_CODE.DissconectionFlag) {
-            StateManager.instance().eventSystem.AgentDisconnected(this, true);
-        }
+        protocol.Receive();
     }
 
 }
